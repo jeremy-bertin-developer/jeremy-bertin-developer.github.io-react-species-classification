8fdfaea62b07d229e7e3e215a31523fe87f451e5
Jeremy Bertin <jeremy.bertin76@hotmail.fr>
2020-12-01 21:34:20 +0100

    first commit

---
+++ precache-manifest.d52a477e7e525d44f4d9e9ccd135dba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608a17355091c4b8e67842a147b92f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc94a668fa47d0167d20",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "6889c776cb7e8df123e6",
+    "url": "/static/css/main.030e31bb.chunk.css"
+  },
+  {
+    "revision": "dc94a668fa47d0167d20",
+    "url": "/static/js/2.17a954d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.17a954d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6889c776cb7e8df123e6",
+    "url": "/static/js/main.dd3c9c2d.chunk.js"
+  },
+  {
+    "revision": "7916170adef8a060f306",
+    "url": "/static/js/runtime-main.062ba4ad.js"
+  }
+]);
